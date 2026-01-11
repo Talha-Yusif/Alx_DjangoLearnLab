bd00fdbfa23c9c3868f5fa87347e2ef4cd8dbbfb
Talha Yusif <talhaysif06@gmail.com>
2026-01-11 14:09:29 +0000

    bookshelf

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Book
 
 class BookAmdin(admin.ModelAdmin):
-    list_display=('title','order','publication_year')
-    search_fields=('title')
+    list_display=('title','author','publication_year')
+    search_fields=('title','publication_year')
     list_filter=('title','publication_year')
+admin.site.register(Book,BookAmdin)
