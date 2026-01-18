909ce4853c4021b72b22a5e1144eb85e93c3af5d
Talha Yusif <talhaysif06@gmail.com>
2026-01-18 23:33:15 +0000

    logout,login and registeration

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
+from .views import register, UserLoginView, UserLogoutView
 urlpatterns=[
     path('books/',list_books,name="book_list"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+    path("register/", register, name="register"),
 ]
