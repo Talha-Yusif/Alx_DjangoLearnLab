ec5b6e9382f9669b22f3bc16a3db7dadbacc707a
Talha Yusif <talhaysif06@gmail.com>
2026-01-18 18:38:37 +0000

    django urls and views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Book
 from django.views.generic.detail import DetailView
 
 
-def book_list(request):
+def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {'books': books})
 
