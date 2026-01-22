125eca46340f61059d04c2a18b541ac7c86f0470
Talha Yusif <talhaysif06@gmail.com>
2026-01-22 09:49:32 +0000

    Update

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,51 @@
-from django.shortcuts import render
-from .models import Library
-from .models import Book
-from django.views.generic.detail import DetailView
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.detail import DetailView
+
+from .models import Library, Book
+
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
+
 
 
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {'books': books})
 
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
 
 def register(request):
     if request.method == "POST":
@@ -27,8 +57,11 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+
+
 class UserLoginView(LoginView):
     template_name = "relationship_app/login.html"
+
+
 class UserLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
-
