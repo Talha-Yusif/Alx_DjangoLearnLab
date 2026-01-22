995dd7e1c40cab8a942da7d4c6e5c217dd779afb
Talha Yusif <talhaysif06@gmail.com>
2026-01-22 10:10:21 +0000

    updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.detail import DetailView
 
 from .models import Library, Book
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Book
+
 
 
 
@@ -65,3 +69,29 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     template_name = "relationship_app/logout.html"
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        author_id = request.POST.get("author")
+        Book.objects.create(title=title, author_id=author_id)
+        return redirect("book_list")
+    return render(request, "relationship_app/add_book.html")
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.title = request.POST.get("title")
+        book.save()
+        return redirect("book_list")
+    return render(request, "relationship_app/edit_book.html", {"book": book})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        book.delete()
+        return redirect("book_list")
+    return render(request, "relationship_app/delete_book.html", {"book": book})
